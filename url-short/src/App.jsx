034adaa10ae1a68b6f5248e05b5aebaeb231c987
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2024-09-08 18:23:08 +0530

    first commit upto login ui

---
+++ url-short/src/App.jsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { Button } from "@/components/ui/button";
+import AppLayout from "./layouts/AppLayout";
+import LandingPage from "./pages/Landing";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
+import UrlProvider from "./context";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link/:id",
+        element: <Link />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
+}
+
+export default App;
