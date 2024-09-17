7bc9bce29e3805f0b03f66bb395ed7f87a4ae353
munnavuyyuru <111678429+munnavuyyuru@users.noreply.github.com>
2024-09-17 23:25:58 +0530

    link.jsx is pending

--- url-short/src/db/apiClicks.js
+++ url-short/src/db/apiClicks.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+import UAParser from "ua-parser-js";
 
 export async function getClicksForUrls(urlIds) {
   const { data, error } = await supabase
@@ -13,3 +14,26 @@ export async function getClicksForUrls(urlIds) {
 
   return data;
 }
+
+const parser = new UAParser();
+
+export const storeClicks = async ({ id, originalUrl }) => {
+  try {
+    const res = parser.getResult();
+    const device = res.type || "desktop";
+
+    const response = await fetch("https://ipapi.co/json");
+    const { city, country_name: country } = await response.json();
+
+    await supabase.from("clicks").insert({
+      url_id: id,
+      city: city,
+      country: country,
+      device: device,
+    });
+
+    window.location.href = originalUrl;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
--- url-short/src/pages/link.jsx
+++ url-short/src/pages/link.jsx
@@ -1,7 +1,80 @@
+import { UrlState } from "@/context";
+import { getUrl } from "@/db/apiUrls";
+import useFetch from "@/hooks/use-fetch";
 import React from "react";
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+import { getClicksForUrl } from "@/db/apiUrls";
+import { deleteUrl } from "@/db/apiUrls";
+import { LinkIcon } from "lucide-react";
 
 const Link = () => {
-  return <div>Link</div>;
+  const { id } = useParams();
+  const { user } = UrlState();
+  const navigate = useNavigate();
+
+  const {
+    loading,
+    data: url,
+    fn,
+    error,
+  } = useFetch(getUrl, { id, user_id: user?.id });
+
+  const {
+    loading: loadingStats,
+    data: stats,
+    fn: fnStats,
+  } = useFetch(getClicksForUrl, id);
+
+  const { loading: loadingDelete, fn: fnDelete } = useFetch(deleteUrl, id);
+
+  useEffect(() => {
+    fn();
+  }, []);
+
+  if (error) {
+    navigate("/dashboard");
+  }
+
+  let link = "";
+  if (url) {
+    link = url?.custom_url ? url?.custom_url : url.short_url;
+  }
+
+  return (
+    <>
+      {(loading || loadingStats) && (
+        <BarLoader className="mb-4 ml-5" width={"100%"} color="#36d7b7" />
+      )}
+      <div className="ml-8 flex flex-col gap-8 sm:flex-row justify-between">
+        <div className="flex flex-col items-start gap-8 rounded-lg sm:w-2/5">
+          <span className="text-6xl font-extrabold hover:underline cursor-pointer">
+            {url?.title}
+          </span>
+          <a
+            href={`https://trimrr.in/${link}`}
+            target="_blank"
+            className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
+          >
+            https://trimrr.in/{link}
+          </a>
+          <a
+            href={url?.original_url}
+            target="_blank"
+            className="flex items-center gap-1 hover:underline cursor-pointer"
+          >
+            <LinkIcon className="p-1" />
+            {url?.original_url}
+          </a>
+          <span className="flex items-end font-extralight text-sm">
+            {new Date(url?.created_at).toLocaleString()}
+          </span>
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Link;
